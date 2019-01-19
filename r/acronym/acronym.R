f987ffc6e5c532b4d2d6566c8a75e7f797e2e9a4
y0wel <matz_tobias@web.de>
2019-01-19 23:32:36 +0100

    solution for exercise acronym

--- r/acronym/acronym.R
+++ r/acronym/acronym.R
@@ -1,3 +1,10 @@
 acronym <- function(input) {
-    
+  input <- gsub("-", " ", input)
+  input <- gsub(",", "", input)
+  input <- strsplit(input, split = " ")[[1]]
+  output <- c()
+  for(i in 1:length(input)) {
+    output <- append(output, toupper(substr(input[i], 1, 1)))
+  }
+  paste0(output, collapse = "")
 }
